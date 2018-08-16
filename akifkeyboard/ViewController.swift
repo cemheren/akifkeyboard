cbcfa01de82de78cfc9a212ec5f1b9421ff43488
cemheren <cemheren@gmail.com>
2018-08-15 21:45:35 -0700

    Initial Commit

---
+++ akifkeyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  akifkeyboard
+//
+//  Created by Akif Heren on 8/15/18.
+//  Copyright © 2018 Akif Heren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
